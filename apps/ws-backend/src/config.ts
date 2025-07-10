15adfd841283319ccf5326acddc039cc44b6a631
Om <144691499+ombalgude@users.noreply.github.com>
2025-07-10 18:09:53 +0530

    fixing config to pass jwt_secret

--- apps/ws-backend/src/config.ts
+++ apps/ws-backend/src/config.ts
@@ -10,4 +10,4 @@ if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET must be defined");
 }
 
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_SECRET = process.env.JWT_SECRET || "";
